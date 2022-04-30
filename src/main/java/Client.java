158b7f87a8108adb2189b20d55a6d3d5e4be6aa7
Пётр Тетерин <petr200700@mail.ru>
2022-04-30 17:26:38 +0700

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        String host = "127.0.0.1";
+        int port = 8088;
+
+        String name = "Petr";
+
+        try (
+                Socket clientSocket = new Socket(host, port);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+
+            System.out.println("Connected to " + clientSocket.getRemoteSocketAddress());
+
+            out.println(name);
+
+            String resp = in.readLine();
+
+            System.out.println("Server response: " + resp);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
